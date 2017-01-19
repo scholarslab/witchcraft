60dc7060fed67cfb696292feec43be2692ac10b8
Tingdan Luo <365752181@qq.com>
2017-01-19 00:23:38 -0500

    01/09/2017

---
+++ generateString.py
@@ -0,0 +1,7 @@
+
+
+s1 = ".page-id-"
+s3 = " .site-header,"
+for i in range(4801, 4850, 2):
+
+	print s1 + str(i) + s3
---
+++ printPage.py
@@ -0,0 +1,33 @@
+s = '<p style="text-align: center; margin-top: -10px;"><font size="+1">Records and Files of the Quarterly Courts of Essex County</font></p>
+<hr width="35" />
+<p style="text-align: center; margin-top: -20px; line-height: 1.2;"><font size="3px">Volume I</font></p>
+<div style="width: 95%; margin: 0 auto;">
+<table border="1" cellspacing="0" cellpadding="4">
+<tbody>
+<tr>
+<td colspan="1" rowspan="1" width="30"><center><a href="http://localhost/wordpress/index.php/documents/document-2/vol1/vo1-contents/" shape="rect">&lt;&lt;</a></center></td>
+<td colspan="1" rowspan="1"></td>
+<td colspan="1" rowspan="1" width="30"><center><a href="http://localhost/wordpress/index.php/documents/document-2/vol1/vo1-contents/vol1-228/" shape="rect">&lt;</a></center></td>
+<td colspan="1" rowspan="1" width="30"><center><a href="http://localhost/wordpress/index.php/documents/document-2/vol1/vo1-contents/vol1-230/" shape="rect">&gt;</a></center></td>
+</tr>
+</tbody>
+</table>
+</table>
+</div>
+<center style="margin-top: -40px">
+<img src="http://localhost/wordpress/wp-content/uploads/2017/01/essex229.gif" />
+</center>
+<div style="width: 95%; margin: 0 auto; margin-top: -10px;">
+<table border="1" cellspacing="0" cellpadding="4">
+<tbody>
+<tr>
+<td colspan="1" rowspan="1" width="30"><center><a href="http://localhost/wordpress/index.php/documents/document-2/vol1/vo1-contents/" shape="rect">&lt;&lt;</a></center></td>
+<td colspan="1" rowspan="1"></td>
+<td colspan="1" rowspan="1" width="30"><center><a href="http://localhost/wordpress/index.php/documents/document-2/vol1/vo1-contents/vol1-228/" shape="rect">&lt;</a></center></td>
+<td colspan="1" rowspan="1" width="30"><center><a href="http://localhost/wordpress/index.php/documents/document-2/vol1/vo1-contents/vol1-230/" shape="rect">&gt;</a></center></td>
+</tr>
+</tbody>
+</table>
+</div>'
+
+print s
